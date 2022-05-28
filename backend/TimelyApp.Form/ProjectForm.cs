f4175f843a0c71ba12e36d320003914f6f7137f7
Alan Emeršić <alan.emersic@gmail.com>
2022-05-28 13:38:40 +0200

    add gitignore

--- backend/TimelyApp.Form/ProjectForm.cs
+++ backend/TimelyApp.Form/ProjectForm.cs
@@ -5,12 +5,12 @@ namespace TimelyApp.Form
     public class ProjectForm
     {
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required]
-        public string StartTime { get; set; }
+        public string? StartTime { get; set; }
 
         [Required]        
-        public string EndTime { get; set; }
+        public string? EndTime { get; set; }
     }
 }
